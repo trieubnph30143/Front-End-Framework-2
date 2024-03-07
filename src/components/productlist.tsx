55d6c9b126e1cad9aec104b6d11616531a6b21c4
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-03-07 22:35:24 +0700

    B2

--- src/components/productlist.tsx
+++ src/components/productlist.tsx
@@ -13,10 +13,10 @@ const Productlist = (props: Props) => {
     <div className="content">
       <div className="container">
         <div className="row">
-          {products.map((product: any, index: number) => {
+          {products.map((product: IProduct, index: number) => {
             return (<div key={product.id} className="item col-6 col-sm-4 col-md-3">
               <img src={product.image} />
-              <h3>{product.title}</h3>
+              <h3>{product.name}</h3>
               <span>{product.price}</span>
               <p><Addtocart pid={product.id}/></p>
             </div>)
