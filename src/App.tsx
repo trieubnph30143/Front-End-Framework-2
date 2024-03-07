571ec6ddab2f87082f663dac212030a638907836
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-03-07 20:26:28 +0700

    B2

--- src/App.tsx
+++ src/App.tsx
@@ -8,33 +8,32 @@ import Footer from './components/footer';
 import IProduct from './interface/product';
 function App() {
   const title:string = "Xin chào";
-  const products:IProduct[]=[]
-  // const products:IProduct[]=[
-  //   {
-  //     "id":1,
-  //     "name":"Sản phẩm 1",
-  //     "image": "https://img.muji.net/img/item/4550344275726_1260.jpg",
-  //     "price":10000
-  //   },
-  //   {
-  //     "id":2,
-  //     "name":"Sản phẩm 1",
-  //     "image": "https://giadungnhaviet.com/wp-content/uploads/2019/12/dong-ho-treo-tuong-casio-nhat-ban-IQ-58-7JF-5.jpg",
-  //     "price":10000
-  //   },
-  //   {
-  //     "id":3,
-  //     "name":"Sản phẩm 1",
-  //     "image": "https://donghoquanglam.com/thumb/grande/100/408/269/products/2-1b3fc0cb-6295-4167-82b4-7ae89c68def0-b3dc9a0c-1cd7-4ef1-94ca-c42bf110847d-0ae73981-c1b1-433e-91c1-85358d79e724.png?v=1683009883150",
-  //     "price":10000
-  //   },
-  //   {
-  //     "id":4,
-  //     "name":"Sản phẩm 1",
-  //     "image": "https://wscdn.vn/400x400/upload/original-image/iQ-151-5DF-1.jpg",
-  //     "price":10000
-  //   }
-  // ]
+  const products:IProduct[]=[
+    {
+      "id":1,
+      "name":"Sản phẩm 1",
+      "image": "https://img.muji.net/img/item/4550344275726_1260.jpg",
+      "price":10000
+    },
+    {
+      "id":2,
+      "name":"Sản phẩm 1",
+      "image": "https://giadungnhaviet.com/wp-content/uploads/2019/12/dong-ho-treo-tuong-casio-nhat-ban-IQ-58-7JF-5.jpg",
+      "price":10000
+    },
+    {
+      "id":3,
+      "name":"Sản phẩm 1",
+      "image": "https://donghoquanglam.com/thumb/grande/100/408/269/products/2-1b3fc0cb-6295-4167-82b4-7ae89c68def0-b3dc9a0c-1cd7-4ef1-94ca-c42bf110847d-0ae73981-c1b1-433e-91c1-85358d79e724.png?v=1683009883150",
+      "price":10000
+    },
+    {
+      "id":4,
+      "name":"Sản phẩm 1",
+      "image": "https://wscdn.vn/400x400/upload/original-image/iQ-151-5DF-1.jpg",
+      "price":10000
+    }
+  ]
   return (
     <>
       <Header title= {title}/>
--- src/components/productlist.tsx
+++ src/components/productlist.tsx
@@ -1,40 +1,37 @@
-import React from 'react'
-import IProduct from '../interface/product';
+import React from "react";
+import IProduct from "../interface/product";
+import Addtocart from "./addtocart";
 
 type Props = {
-    products: IProduct[];
-}
+  products: IProduct[];
+};
 
 const Productlist = (props: Props) => {
-    const products:any[]=props.products;
-    console.log(products);
+  const products: any[] = props.products;
+  console.log(products);
   return (
     <div className="content">
-  <div className="container">
-    <div className="row">
-      <div className="item col-6 col-sm-4 col-md-3">
-          <img src="https://img.muji.net/img/item/4550344275726_1260.jpg"/>
-          <h3>Sản phẩm số 1</h3>
-          <span>1000</span>
+      <div className="container">
+        <div className="row">
+          {products.map((product: any, index: number) => {
+            return (<div key={product.id} className="item col-6 col-sm-4 col-md-3">
+              <img src={product.image} />
+              <h3>{product.title}</h3>
+              <span>{product.price}</span>
+              <p><Addtocart pid={product.id}/></p>
+            </div>)
+          })}
+        </div>
       </div>
-      <div className="item col-6 col-sm-4 col-md-3">
-          <img src="https://img.muji.net/img/item/4550344275726_1260.jpg"/>
-          <h3>Sản phẩm số 1</h3>
-          <span>1000</span>
-      </div>
-      <div className="item col-6 col-sm-4 col-md-3">
-          <img src="https://img.muji.net/img/item/4550344275726_1260.jpg"/>
-          <h3>Sản phẩm số 1</h3>
-          <span>1000</span>
-      </div>
-      <div className="item col-6 col-sm-4 col-md-3">
-          <img src="https://img.muji.net/img/item/4550344275726_1260.jpg"/>
-          <h3>Sản phẩm số 1</h3>
-          <span>1000</span>
-      </div>
-      </div>
-  </div>
-</div>
-  )
-}
+    </div>
+  );
+};
 export default Productlist;
+
+{
+  /* <div className="item col-6 col-sm-4 col-md-3">
+          <img src="https://img.muji.net/img/item/4550344275726_1260.jpg"/>
+          <h3>Sản phẩm số 1</h3>
+          <span>1000</span>
+      </div> */
+}
